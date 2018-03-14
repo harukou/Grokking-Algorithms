fa4518275801da4faec631ff3456b9686caeb2f0
Chunhao Wang <37358087+harukou@users.noreply.github.com>
2018-03-14 22:30:20 +0800

    Add files via upload
    greatest common divisor  递归+循环

---
+++ Recursion/gcd.py
@@ -0,0 +1,15 @@
+def gcd_recursion(x,y):
+	return x if y == 0 else gcd_recursion(y,x%y)
+
+def gcd(x,y):
+	while y:
+		x,y = y,x%y
+	return x
+
+x = int(input('x='))
+y = int(input('y='))
+
+print(gcd_recursion(x,y))
+print(gcd(x,y))
+
+	
