cbfe417229a6861ce3ebfe4f7991e62eb88e3269
Chunhao Wang <37358087+harukou@users.noreply.github.com>
2018-03-14 21:40:49 +0800

    two way: recursion and loop

---
+++ Recursion/findBiggest.py
@@ -0,0 +1,20 @@
+def max(arr):
+	if len(arr) == 2:
+		return arr[0] if arr[0]>arr[1] else arr[1]
+	sub_max = max(arr[1:])
+	return arr[0] if arr[0]>sub_max else sub_max
+
+arr = [342,5,46,554,745,6]
+
+print('recursion max:',max(arr))
+
+def max1(arr):
+	max = arr[0]
+	for x in range(1,len(arr)-1):
+		if max<arr[x]:
+			max = arr[x]
+	return max
+
+arr = [342,5,46,554,745,6]
+
+print('loop max:',max1(arr))
